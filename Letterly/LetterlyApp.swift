3758c33ef9a4d06bc044778525da6419e3bafe84
Kondiko21 <koplociennik@gmail.com>
2024-02-18 17:17:05 +0100

    Initial Commit

---
+++ Letterly/LetterlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetterlyApp.swift
+//  Letterly
+//
+//  Created by Konrad on 18/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct LetterlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
