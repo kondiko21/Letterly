496c99e00abce3d9cfac5c616119c58f70531713
Kondiko21 <koplociennik@gmail.com>
2024-03-26 18:57:51 +0100

    Added game over view and main view

---
+++ Letterly/CustomModifiers.swift
@@ -0,0 +1,18 @@
+//
+//  CustomModifiers.swift
+//  Letterly
+//
+//  Created by Konrad on 13/03/2024.
+//
+
+import SwiftUI
+
+struct CustomModifiers: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomModifiers()
+}
---
+++ Letterly/CustomSegmentedPicker.swift
@@ -0,0 +1,18 @@
+//
+//  CustomSegmentedPicker.swift
+//  Letterly
+//
+//  Created by Konrad on 14/03/2024.
+//
+
+import SwiftUI
+
+struct CustomSegmentedPicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomSegmentedPicker()
+}
